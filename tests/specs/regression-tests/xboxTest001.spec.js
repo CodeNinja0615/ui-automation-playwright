fcb8c465f1d5b7ff4e7cf4356e841d18beaaabc0
CodeNinja0615 <akhtarsameer743@gmail.com>
2026-02-05 02:09:14 +0530

    working added regression test

---
+++ tests/specs/regression-tests/xboxTest001.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+import { PageObjectManager } from "../../page-objects/PageObjectManager";
+import { LandingPage } from "../../page-objects/LandingPage";
+import envJson from "../../../test-data/url.json" with { type: "json" };
+import data_set from "../../../test-data/xboxTest101.json" with { type: "json" };
+import "../../hooks/globalHooks";
+const username = data_set.username;
+const envData = envJson[0];
+const timeout = envJson[0].timeout;
+/** @type {LandingPage} */
+let landingPage;
+
+test.describe.configure({mode: 'serial'});
+test.describe(`@Regression`, async () => {
+    test.beforeAll(``, async ({ browser }) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        const pom = new PageObjectManager(page);
+        landingPage = pom.getLandingPage();
+        const password = landingPage.getCredentials(username);
+        await landingPage.loginApplication(envData.login_url, username, password);
+        await landingPage.homePage().waitFor({ state: 'visible', timeout: timeout.mid });
+        expect(await landingPage.homePage().isVisible()).toBe(true);
+    });
+});
